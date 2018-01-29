cc990215ffb36972492de0717c3c156723429c10
Zach Gulde <zachgulde@gmail.com>
2018-01-29 13:39:54 -0600

    Bootstrap 4

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 /**
- * es6 modules and imports
+ * This file is the "entrypoint" into your application
  */
-import sayHello from './hello';
-sayHello('World');
+import 'bootstrap'
+import $ from 'jquery'
 
-/**
- * require style imports
- */
-const {getMovies} = require('./api.js');
+$(() => {
+  $('[data-toggle="popover"]').popover()
+})
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
