d6140547f87b9766ea2c88cb704b581309b35f51
Timryan133 <Tim.ryan133@gmail.com>
2019-03-14 16:14:10 -0500

    added connection to Json

--- src/index.js
+++ src/index.js
@@ -15,12 +15,40 @@ getMovies().then((movies) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
 
+/*--Sumbit button for submitting movies to the backend---------------------------------------------------------------------------------------- */
 $('#mSubmit').click(
     function() {
       console.log($('#userNameInput').val());
     }
 );
+
+/*------------------------------------------------------------------------------------------ */
+
+
+function add_Movie() {
+    const movie_info = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
+    const url = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movie_info),
+    };
+    fetch(url, options)
+        .then(console.log("post was created successfully"))
+        .catch(console.log(" handle errors "));
+}
+
+add_Movie();
+
+// "title": "Ajax Requests",
+//     "body": "Are a fun way to use JS!",
+//     "id": 4
+
+
+
