6be3629c8300537570b96166309f59b1b61366d8
AdrienVL <adrienlhemann@gmail.com>
2019-03-15 12:01:34 -0500

    API successfully integrated

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ sayHello('World');
 /**
  * require style imports
  */
+
+
+//------------------------------------------------------console log database----------------------------------------------------------//
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
@@ -19,6 +22,54 @@ getMovies().then((movies) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
 });
+
+
+
+//--------------------------------------------------Add poster images to movies using movieDB database------------------------------//
+
+
+function addImage () {
+
+    const film = $('#movieTitle').val();
+
+    if(film === ''){
+
+        //Empty input submission
+        $('#movieTitle').html('<div class="alert"><strong>Oops!</strong> Try adding something into the search field.</div>');
+
+    } else {
+
+
+        $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function (json) {
+
+            //If successful
+            if (json != "Nothing found.") {
+                console.log(json);
+
+                $('.earth-container').append('<p>Your search found: <strong>' + json.results[0].title + '</strong></p><img src=\"http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + '\" class=\"img-responsive\" >');
+
+                add_Movie();
+
+            } else {
+                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=goonies&callback=?", function(json) {
+
+                    console.log(json);
+                    $('#poster').html('<div class="alert"><p>We\'re afraid nothing was found for that search.</p></div><p>Perhaps you were looking for The Goonies?</p><img id="thePoster" src="http://image.tmdb.org/t/p/w500/' + json[0].poster_path + ' class="img-responsive" />');
+                });
+            }
+
+        })
+
+    }
+
+    return false;
+
+
+}
+
+//Add movie to the databsed after
+
+
 
 function add_Movie() {
     const movie_info = {title: $('#movieTitle').val(), rating: $('#movieRating').val()};
@@ -39,7 +90,7 @@ function add_Movie() {
 
 $('#mSubmit').click(
     function () {
-        add_Movie();
+        addImage();
     }
 );
 
@@ -54,8 +105,6 @@ $('#mSubmit').click(
 
 //
 
-var poster = $.getJSON("https://api.themoviedb.org/3/discover/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb");
-console.log(poster);
 
 $.get("/api/movies").done(function (data) {
 
