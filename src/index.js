9bb3db7ed1168f8d2433b9e64643c6817cbf88e6
AdrienVL <adrienlhemann@gmail.com>
2019-03-16 20:20:08 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -43,14 +43,18 @@ function addImage() {
 
             //If successful
             if (json !== "Nothing found.") {
-                console.log(json);
+
 
                 //Append movie title and movie img
 
+                console.log(typeof json.results);
                 $('.earth-container').append(json.results[0].title + '<img src=\"http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + '\" class=\"img-responsive\" >');
+
 
                 //Existing movie, so add to database
                 add_Movie();
+
+
 
             }
 
@@ -66,6 +70,8 @@ function addImage() {
 
 //--------------------------------Add movie to the databse after add Image is successful using post----------------------------------//
 
+
+//
 
 function add_Movie() {
     const movie_info = {title: $('#movieTitle').val(), rating: $('#movieRating').val()};
@@ -83,15 +89,50 @@ function add_Movie() {
         .catch(console.log(" handle errors "));
 }
 
-/*--Sumbit button for submitting movies to the backend---------------------------------------------------------------------------------------- */
+
+//Tie it to the grid system.
+
+function deleteMovie() {
+
+
+    $.get("/api/movies").done(function (data){
+        console.log(data[0].id);
+        //url to the database
+        const url = '/api/movies';
+        const options = {
+            method: 'DELETE',
+        };
+        fetch(url, options)
+            .then(console.log("post was created successfully"))
+            .catch(console.log(" handle errors "));
+    })
+}
+
+
+
+
+
+
+
+/*--Sumbit button for submitting movies to the database---------------------------------------------------------------------------------------- */
 
 $('#mSubmit').click(
     function () {
         addImage();
-    }
-);
 
-/*---------------------------------------------------Appending movies---------------------------------------------------- */
+    }
+)
+
+
+$('#delete').click(
+    function () {
+        deleteMovie();
+
+    }
+)
+
+
+/*---------------------------------------------------Appending movies/Displaying Movies---------------------------------------------------- */
 
 
 
@@ -100,16 +141,25 @@ $.get("/api/movies").done(function (data) {
 
     data.forEach(function (data) {
 
-        $('.display-movies').append('<div class="earth">' +
-            '<div class="earth-wrapper">' +
-            '<div class="earth-container">' +
-            data.title
-            + '</div>');
+        const film = data.title;
 
-    })
+        $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + film + "&callback=?", function (json) {
 
 
-});
+            console.log(json.results[0].poster_path);
+            $('.display-movies').append('<div class="earth">' +
+                '<div class="earth-wrapper">' +
+                '<div class="earth-container">' +
+                '<div class="posterImage">' + $('.earth-container').append(data.title + '<img src=\"http://image.tmdb.org/t/p/w500/' + json.results[0].poster_path + '\" class=\"img-responsive\" >') + '</div>'
+                + '</div>');
 
 
-//hi
+        });
+
+    });
+})
+
+
+
+
+
